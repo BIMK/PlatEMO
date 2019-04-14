ccd0f59333cfbdcd34e68cf560a80943422ceb97
severuspeng <pengshichenno.1@live.com>
2019-04-14 11:15:30 +0800

    add new algorithms, problems and metrics

---
+++ PlatEMO/Algorithms/MOEA-D-FRRMAB/FRRMAB.m
@@ -0,0 +1,19 @@
+function op = FRRMAB(FRR,SW,C)
+% Bandit-based operator selection
+
+%------------------------------- Copyright --------------------------------
+% Copyright (c) 2018-2019 BIMK Group. You are free to use the PlatEMO for
+% research purposes. All publications which use this platform or any code
+% in the platform should acknowledge the use of "PlatEMO" and reference "Ye
+% Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB platform
+% for evolutionary multi-objective optimization [educational forum], IEEE
+% Computational Intelligence Magazine, 2017, 12(4): 73-87".
+%--------------------------------------------------------------------------
+
+    if any(FRR==0) || any(SW(1,:)==0)
+        op = randi(length(FRR));
+    else
+        n      = hist(SW(1,:),1:length(FRR));
+        [~,op] = max(FRR+C*sqrt(2*log(sum(n))./n));
+    end
+end
---
+++ PlatEMO/Algorithms/MOEA-D-FRRMAB/FourDE.m
@@ -0,0 +1,50 @@
+function Offspring = FourDE(op,x,x1,x2,x3,x4,x5)
+% Four different DE operators
+
+%------------------------------- Copyright --------------------------------
+% Copyright (c) 2018-2019 BIMK Group. You are free to use the PlatEMO for
+% research purposes. All publications which use this platform or any code
+% in the platform should acknowledge the use of "PlatEMO" and reference "Ye
+% Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB platform
+% for evolutionary multi-objective optimization [educational forum], IEEE
+% Computational Intelligence Magazine, 2017, 12(4): 73-87".
+%--------------------------------------------------------------------------
+
+    %% Parameter setting
+    [CR,F,proM,disM,K] = deal(1,0.5,1,20,0.5);
+    D      = length(x.dec);
+    Global = GLOBAL.GetObj();
+
+    %% Differental evolution
+    switch op
+        case 1
+            % DE/rand/1
+            v = x.dec + F*(x1.dec-x2.dec);
+        case 2
+            % DE/rand/2
+            v = x.dec + F*(x1.dec-x2.dec) + F*(x3.dec-x4.dec);
+        case 3
+            % DE/current-to-rand/2
+            v = x.dec + K*(x.dec-x1.dec) + F*(x2.dec-x3.dec) + F*(x4.dec-x5.dec);
+        case 4
+            % DE/current-to-rand/1
+            v = x.dec + K*(x.dec-x1.dec) + F*(x2.dec-x3.dec);
+    end
+    Offspring = x.dec;
+    Site      = rand(1,D) < (CR+(op>2));
+    Offspring(Site) = v(Site);
+
+    %% Polynomial mutation
+    Lower = Global.lower;
+    Upper = Global.upper;
+    Site  = rand(1,D) < proM/D;
+    mu    = rand(1,D);
+    temp  = Site & mu<=0.5;
+    Offspring       = min(max(Offspring,Lower),Upper);
+    Offspring(temp) = Offspring(temp)+(Upper(temp)-Lower(temp)).*((2.*mu(temp)+(1-2.*mu(temp)).*...
+                      (1-(Offspring(temp)-Lower(temp))./(Upper(temp)-Lower(temp))).^(disM+1)).^(1/(disM+1))-1);
+    temp = Site & mu>0.5; 
+    Offspring(temp) = Offspring(temp)+(Upper(temp)-Lower(temp)).*(1-(2.*(1-mu(temp))+2.*(mu(temp)-0.5).*...
+                      (1-(Upper(temp)-Offspring(temp))./(Upper(temp)-Lower(temp))).^(disM+1)).^(1/(disM+1)));
+    Offspring = INDIVIDUAL(Offspring);
+end
