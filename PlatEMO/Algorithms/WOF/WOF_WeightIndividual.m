0808d98f6a7dc6047b4065e0bed6a9b8d08e5b33
severuspeng <pengshichenno.1@live.com>
2020-05-20 23:42:35 +0800

    delete WOF-SMPSO add WOF

--- PlatEMO/Algorithms/WOF/WOF_WeightIndividual.m
+++ PlatEMO/Algorithms/WOF/WOF_WeightIndividual.m
@@ -1,51 +1,62 @@
 classdef WOF_WeightIndividual < handle
 % WOF_WeightIndividual - The class of an individual used in WOF to store
-% weight variables. It is derived from the "INDIVIDUAL" class of the
-% PlatEMO framework and adjusted to fit the WOF algorithm. 
+% weight variables. 
 
 % ----------------------------------------------------------------------- 
-%  WOF_WeightIndividual.m 
-%  Copyright (C) 2018 Heiner Zille
+%  Copyright (C) 2020 Heiner Zille
+%
+%  This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 
+%  International License. (CC BY-NC-SA 4.0). To view a copy of this license, 
+%  visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or see the 
+%  pdf-file "License-CC-BY-NC-SA-4.0.pdf" that came with this code. 
+%
+%  You are free to: 
+%  * Share ? copy and redistribute the material in any medium or format
+%  * Adapt ? remix, transform, and build upon the material 
+%  Under the following terms:
+%  * Attribution ? You must give appropriate credit, provide a link to the 
+%     license, and indicate if changes were made. You may do so in any reasonable 
+%     manner, but not in any way that suggests the licensor endorses you or your use.
+%  * NonCommercial ? You may not use the material for commercial purposes.
+%  * ShareAlike ? If you remix, transform, or build upon the material, you must 
+%    distribute your contributions under the same license as the original.
+%  * No additional restrictions ? You may not apply legal terms or technological 
+%    measures that legally restrict others from doing anything the license permits.
 % 
-%  This program is free software: you can redistribute it and/or modify
-%  it under the terms of the GNU General Public License as published by
-%  the Free Software Foundation, either version 3 of the License, or
-%  (at your option) any later version.
-%
-%  This program is distributed in the hope that it will be useful,
-%  but WITHOUT ANY WARRANTY; without even the implied warranty of
-%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%  GNU General Public License for more details.
-%
-%  You should have received a copy of the GNU General Public License
-%  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-%
 %  Author of this Code: 
-%   Heiner Zille <heiner.zille@ovgu.de>
+%   Heiner Zille <heiner.zille@ovgu.de> or <heiner.zille@gmail.com>
 %
-%  This file belongs to the following publications:
-%
-%  1) Heiner Zille and Sanaz Mostaghim
+%  This code is based on the following publications:
+% 
+%  1) Heiner Zille 
+%     "Large-scale Multi-objective Optimisation: New Approaches and a Classification of the State-of-the-Art"  
+%     PhD Thesis, Otto von Guericke University Magdeburg, 2019 
+%     http://dx.doi.org/10.25673/32063 
+% 
+%  2) Heiner Zille and Sanaz Mostaghim
 %     "Comparison Study of Large-scale Optimisation Techniques on the LSMOP Benchmark Functions"  
 %     IEEE Symposium Series on Computational Intelligence (SSCI), IEEE, Honolulu, Hawaii, November 2017
 %     https://ieeexplore.ieee.org/document/8280974 
 % 
-%  2) Heiner Zille, Hisao Ishibuchi, Sanaz Mostaghim and Yusuke Nojima
+%  3) Heiner Zille, Hisao Ishibuchi, Sanaz Mostaghim and Yusuke Nojima
 %     "A Framework for Large-scale Multi-objective Optimization based on Problem Transformation"
 %     IEEE Transactions on Evolutionary Computation, Vol. 22, Issue 2, pp. 260-275, April 2018.
 %     http://ieeexplore.ieee.org/document/7929324
 %  
-%  3) Heiner Zille, Hisao Ishibuchi, Sanaz Mostaghim and Yusuke Nojima
+%  4) Heiner Zille, Hisao Ishibuchi, Sanaz Mostaghim and Yusuke Nojima
 %     "Weighted Optimization Framework for Large-scale Mullti-objective Optimization"
 %     Genetic and Evolutionary Computation Conference (GECCO), ACM, Denver, USA, July 2016
 %     http://dl.acm.org/citation.cfm?id=2908979
 %
-%  Date of publication: 12.10.2018 
-%  Last Update: 12.10.2018 
+%  This file is intended to work with the PlatEMO framework version 2.5. 
+%  Date of publication of this code: 06.04.2020 
+%  Last Update of this code: 06.04.2020
+%  A newer version of this algorithm may be available. Please contact the author 
+%  or see http://www.ci.ovgu.de/Research/Codes.html. 
+% ----------------------------------------------------------------------- 
+% This file is derived from its original version containied in the PlatEMO 
+% framework. 
 % -----------------------------------------------------------------------
-    
-% Original copyright disclaimer of the "Individual" class of the 
-% PlatEMO framework version 1.5: 
     
     properties(SetAccess = private)
         dec;        % Decision variables of the individual
@@ -63,12 +74,16 @@ classdef WOF_WeightIndividual < handle
                 xPrimeSize = size(xPrimeVars,2);
 
                 obj = WOF_WeightIndividual;
+                
+                % Set the infeasible decision variables to boundary values
+                variables  = max(min(variables,GlobalDummy.upper),GlobalDummy.lower);
 
-                x = 1:xPrimeSize;
-                for j = 1:xPrimeSize
-                    x(j) = WOF_transformationFunction(xPrimeVars(j),variables(GlobalDummy.G(j)),GlobalDummy.xPrimeupper(j),GlobalDummy.xPrimelower(j), GlobalDummy.psi);
-                end
 
+                
+                x = WOF_transformationFunctionMatrixForm(xPrimeVars,variables(GlobalDummy.G),GlobalDummy.xPrimeupper,GlobalDummy.xPrimelower, GlobalDummy.psi);
+
+
+                
                 obj.dec = variables;
                 obj.ind = INDIVIDUAL(x);
                 obj.obj = obj.ind.obj;
--- PlatEMO/Algorithms/WOF/WOF_optimiseBySMPSO.m
+++ PlatEMO/Algorithms/WOF/WOF_optimiseBySMPSO.m
@@ -1,53 +1,65 @@
 function Gbest = WOF_optimiseBySMPSO(GlobalDummy, inputPopulation, evaluations, isDummy)
 % This function performs the optimisation by using the SMPSO
-% methodology. It is derived from the PlatEMO version of SMPSO.
+% methodology. 
 % Additional functionality has been added and modified to make it
 % applicable for normal Individuals as well as the transformed
 % weight-variables. 
 
 % ----------------------------------------------------------------------- 
-%  WOF_optimiseBySMPSO.m 
-%  Copyright (C) 2018 Heiner Zille
+%  Copyright (C) 2020 Heiner Zille
+%
+%  This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 
+%  International License. (CC BY-NC-SA 4.0). To view a copy of this license, 
+%  visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or see the 
+%  pdf-file "License-CC-BY-NC-SA-4.0.pdf" that came with this code. 
+%
+%  You are free to: 
+%  * Share ? copy and redistribute the material in any medium or format
+%  * Adapt ? remix, transform, and build upon the material 
+%  Under the following terms:
+%  * Attribution ? You must give appropriate credit, provide a link to the 
+%     license, and indicate if changes were made. You may do so in any reasonable 
+%     manner, but not in any way that suggests the licensor endorses you or your use.
+%  * NonCommercial ? You may not use the material for commercial purposes.
+%  * ShareAlike ? If you remix, transform, or build upon the material, you must 
+%    distribute your contributions under the same license as the original.
+%  * No additional restrictions ? You may not apply legal terms or technological 
+%    measures that legally restrict others from doing anything the license permits.
 % 
-%  This program is free software: you can redistribute it and/or modify
-%  it under the terms of the GNU General Public License as published by
-%  the Free Software Foundation, either version 3 of the License, or
-%  (at your option) any later version.
-%
-%  This program is distributed in the hope that it will be useful,
-%  but WITHOUT ANY WARRANTY; without even the implied warranty of
-%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%  GNU General Public License for more details.
-%
-%  You should have received a copy of the GNU General Public License
-%  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-%
 %  Author of this Code: 
-%   Heiner Zille <heiner.zille@ovgu.de>
+%   Heiner Zille <heiner.zille@ovgu.de> or <heiner.zille@gmail.com>
 %
-%  This file belongs to the following publications:
-%
-%  1) Heiner Zille and Sanaz Mostaghim
+%  This code is based on the following publications:
+% 
+%  1) Heiner Zille 
+%     "Large-scale Multi-objective Optimisation: New Approaches and a Classification of the State-of-the-Art"  
+%     PhD Thesis, Otto von Guericke University Magdeburg, 2019 
+%     http://dx.doi.org/10.25673/32063 
+% 
+%  2) Heiner Zille and Sanaz Mostaghim
 %     "Comparison Study of Large-scale Optimisation Techniques on the LSMOP Benchmark Functions"  
 %     IEEE Symposium Series on Computational Intelligence (SSCI), IEEE, Honolulu, Hawaii, November 2017
 %     https://ieeexplore.ieee.org/document/8280974 
 % 
-%  2) Heiner Zille, Hisao Ishibuchi, Sanaz Mostaghim and Yusuke Nojima
+%  3) Heiner Zille, Hisao Ishibuchi, Sanaz Mostaghim and Yusuke Nojima
 %     "A Framework for Large-scale Multi-objective Optimization based on Problem Transformation"
 %     IEEE Transactions on Evolutionary Computation, Vol. 22, Issue 2, pp. 260-275, April 2018.
 %     http://ieeexplore.ieee.org/document/7929324
 %  
-%  3) Heiner Zille, Hisao Ishibuchi, Sanaz Mostaghim and Yusuke Nojima
+%  4) Heiner Zille, Hisao Ishibuchi, Sanaz Mostaghim and Yusuke Nojima
 %     "Weighted Optimization Framework for Large-scale Mullti-objective Optimization"
 %     Genetic and Evolutionary Computation Conference (GECCO), ACM, Denver, USA, July 2016
 %     http://dl.acm.org/citation.cfm?id=2908979
 %
-%  Date of publication: 12.10.2018 
-%  Last Update: 12.10.2018
+%  This file is intended to work with the PlatEMO framework version 2.5. 
+%  Date of publication of this code: 06.04.2020 
+%  Last Update of this code: 06.04.2020
+%  A newer version of this algorithm may be available. Please contact the author 
+%  or see http://www.ci.ovgu.de/Research/Codes.html. 
+% ----------------------------------------------------------------------- 
+% This file is derived from its original version containied in the PlatEMO 
+% framework. 
 % -----------------------------------------------------------------------
-
-% Original copyright disclaimer of the "SMPSO" function of the 
-% PlatEMO framework version 1.5: 
 
     %% Generate random population
     Population       = inputPopulation;
@@ -82,13 +94,13 @@ end
 function [Gbest,CrowdDis] = WOF_UpdateGbest(Gbest,N)
     % Update the global best set
     Gbest    = Gbest(NDSort(Gbest.objs,1)==1);
-    CrowdDis = CrowdingDistance(Gbest.objs);
+    CrowdDis = WOFCrowdingDistance(Gbest.objs);
     [~,rank] = sort(CrowdDis,'descend');
     Gbest    = Gbest(rank(1:min(N,length(Gbest))));
     CrowdDis = CrowdDis(rank(1:min(N,length(Gbest))));
 end
 
-function CrowdDis = CrowdingDistance(PopObj)
+function CrowdDis = WOFCrowdingDistance(PopObj)
     % Calculate the crowding distance of each solution in the same front
     [N,M]    = size(PopObj);
     CrowdDis = zeros(1,N);
