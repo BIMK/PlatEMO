7cb53b11b9cbeda13ab38e3eb657ea3bf25d8915
Heiner Zille <heiner.zille@ovgu.de>
2020-05-20 16:16:28 +0200

    This provides and update and extensions to the large-scale Weighted Optimization Framework (WOF). WOF now works with 4 different suboptimisers (NSGA-II, NSGA-III, MOEAD, SMPSO) and a randomised mode which achieves better performance. The update and extensions are based on the recent study in [1]. [1] Heiner Zille, "Large-scale Multi-objective Optimisation: New Approaches and a Classification of the State-of-the-Art", PhD Thesis, Otto von Guericke University Magdeburg, 2019, http://dx.doi.org/10.25673/32063

--- PlatEMO/Algorithms/WOF/WOF_selectxPrimes.m
+++ PlatEMO/Algorithms/WOF/WOF_selectxPrimes.m
@@ -4,50 +4,63 @@ function weightIndList = WOF_selectxPrimes(input,amount, method)
 % Distance values from the first non-dominated front. The second one
 % uses a tournament selection on the population based on
 % Pareto-dominance and Crowding Distance. The third option is 
-% introduced in publication (1), see above, based on
+% introduced in publication (2), see above, based on
 % reference directions for the first m+1 chosen solutions and selects
 % random solutions afterwards.    
 
 % ----------------------------------------------------------------------- 
-%  WOF_selectxPrimes.m 
-%  Copyright (C) 2018 Heiner Zille
+%  Copyright (C) 2020 Heiner Zille
+%
+%  This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 
+%  International License. (CC BY-NC-SA 4.0). To view a copy of this license, 
+%  visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or see the 
+%  pdf-file "License-CC-BY-NC-SA-4.0.pdf" that came with this code. 
+%
+%  You are free to: 
+%  * Share ? copy and redistribute the material in any medium or format
+%  * Adapt ? remix, transform, and build upon the material 
+%  Under the following terms:
+%  * Attribution ? You must give appropriate credit, provide a link to the 
+%     license, and indicate if changes were made. You may do so in any reasonable 
+%     manner, but not in any way that suggests the licensor endorses you or your use.
+%  * NonCommercial ? You may not use the material for commercial purposes.
+%  * ShareAlike ? If you remix, transform, or build upon the material, you must 
+%    distribute your contributions under the same license as the original.
+%  * No additional restrictions ? You may not apply legal terms or technological 
+%    measures that legally restrict others from doing anything the license permits.
 % 
-%  This program is free software: you can redistribute it and/or modify
-%  it under the terms of the GNU General Public License as published by
-%  the Free Software Foundation, either version 3 of the License, or
-%  (at your option) any later version.
-%
-%  This program is distributed in the hope that it will be useful,
-%  but WITHOUT ANY WARRANTY; without even the implied warranty of
-%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%  GNU General Public License for more details.
-%
-%  You should have received a copy of the GNU General Public License
-%  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-%
 %  Author of this Code: 
-%   Heiner Zille <heiner.zille@ovgu.de>
+%   Heiner Zille <heiner.zille@ovgu.de> or <heiner.zille@gmail.com>
 %
-%  This file belongs to the following publications:
-%
-%  1) Heiner Zille and Sanaz Mostaghim
+%  This code is based on the following publications:
+% 
+%  1) Heiner Zille 
+%     "Large-scale Multi-objective Optimisation: New Approaches and a Classification of the State-of-the-Art"  
+%     PhD Thesis, Otto von Guericke University Magdeburg, 2019 
+%     http://dx.doi.org/10.25673/32063 
+% 
+%  2) Heiner Zille and Sanaz Mostaghim
 %     "Comparison Study of Large-scale Optimisation Techniques on the LSMOP Benchmark Functions"  
 %     IEEE Symposium Series on Computational Intelligence (SSCI), IEEE, Honolulu, Hawaii, November 2017
 %     https://ieeexplore.ieee.org/document/8280974 
 % 
-%  2) Heiner Zille, Hisao Ishibuchi, Sanaz Mostaghim and Yusuke Nojima
+%  3) Heiner Zille, Hisao Ishibuchi, Sanaz Mostaghim and Yusuke Nojima
 %     "A Framework for Large-scale Multi-objective Optimization based on Problem Transformation"
 %     IEEE Transactions on Evolutionary Computation, Vol. 22, Issue 2, pp. 260-275, April 2018.
 %     http://ieeexplore.ieee.org/document/7929324
 %  
-%  3) Heiner Zille, Hisao Ishibuchi, Sanaz Mostaghim and Yusuke Nojima
+%  4) Heiner Zille, Hisao Ishibuchi, Sanaz Mostaghim and Yusuke Nojima
 %     "Weighted Optimization Framework for Large-scale Mullti-objective Optimization"
 %     Genetic and Evolutionary Computation Conference (GECCO), ACM, Denver, USA, July 2016
 %     http://dl.acm.org/citation.cfm?id=2908979
 %
-%  Date of publication: 12.10.2018 
-%  Last Update: 12.10.2018
-% -----------------------------------------------------------------------
+%  This file is intended to work with the PlatEMO framework version 2.5. 
+%  Date of publication of this code: 06.04.2020 
+%  Last Update of this code: 06.04.2020
+%  A newer version of this algorithm may be available. Please contact the author 
+%  or see http://www.ci.ovgu.de/Research/Codes.html. 
+% ----------------------------------------------------------------------- 
+
     
     inputSize = size(input,2);
     switch method 
@@ -63,7 +76,7 @@ function weightIndList = WOF_selectxPrimes(input,amount, method)
                     theFront = inFrontNo == i;
                     newPop = input(theFront);
                     FrontNo    = NDSort(newPop.objs,inf);
-                    CrowdDis   = CrowdingDistance(newPop.objs,FrontNo);
+                    CrowdDis   = WOF_CrowdingDistance(newPop.objs,FrontNo);
                     [~,I] = sort(CrowdDis,'descend');
                     until=min(left,size(newPop,2));
                     weightIndList = [weightIndList,newPop(I(1:until))];
@@ -81,13 +94,13 @@ function weightIndList = WOF_selectxPrimes(input,amount, method)
             for i = 1:m
                 vec = zeros(1,m);
                 vec(1,i) = 1;
-                angles = pdist2(vec,objValues,'cosine');
+                angles = pdist2(vec,real(objValues),'cosine');
                 [minAngle,minIndex] = min(angles);
                 weightIndList = [weightIndList,input(minIndex)];
             end
             if size(weightIndList,2) < amount
                 vec = ones(1,m);
-                angles = pdist2(vec,objValues,'cosine');
+                angles = pdist2(vec,real(objValues),'cosine');
                 [minAngle,minIndex] = min(angles);
                 weightIndList = [weightIndList,input(minIndex)];
             end
