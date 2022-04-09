90b5e780189419e5094fe6db35d48a31698d4c44
Annibale Panichella <a.panichella@tudelft.nl>
2022-04-09 11:35:08 +0200

    Update AGEMOEA2.m
    Adding reference and copyright information

--- PlatEMO/Algorithms/Multi-objective optimization/AGE-MOEA-II/AGEMOEA2.m
+++ PlatEMO/Algorithms/Multi-objective optimization/AGE-MOEA-II/AGEMOEA2.m
@@ -1,5 +1,21 @@
 classdef AGEMOEA2 < ALGORITHM
-    % <multi/many> <real/binary/permutation> <constrained/none>
+% <multi/many> <real/binary/permutation> <constrained/none>
+
+% AGE-MOEA-II
+
+%------------------------------- Reference --------------------------------
+% A. Panichella, An Improved Pareto Front Modeling Algorithm for Large-
+% scale Many-Objective Optimization, Genetic and Evolutionary Computation 
+% Conference (GECCO), 2022, July 9-13, Boston, MA, USA
+%------------------------------- Copyright --------------------------------
+% Copyright (c) 2022 BIMK Group. You are free to use the PlatEMO for
+% research purposes. All publications which use this platform or any code
+% in the platform should acknowledge the use of "PlatEMO" and reference "Ye
+% Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB platform
+% for evolutionary multi-objective optimization [educational forum], IEEE
+% Computational Intelligence Magazine, 2017, 12(4): 73-87".
+%--------------------------------------------------------------------------
+
     methods
         function main(Algorithm,Problem)
             %% Generate random population
