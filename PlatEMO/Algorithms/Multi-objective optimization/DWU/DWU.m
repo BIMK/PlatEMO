374615e5fec442c3f3eefb4bc0c33150d2b6b462
Gladston Moreira <87828754+moreirag@users.noreply.github.com>
2023-09-12 14:39:15 -0300

    Update DWU.m
    Update DWU by Gladston Moreira.
    
    Please make a minor update DWU.m to DWU algorithm.
    
    Thank you.
    Gladston Moreira

--- PlatEMO/Algorithms/Multi-objective optimization/DWU/DWU.m
+++ PlatEMO/Algorithms/Multi-objective optimization/DWU/DWU.m
@@ -21,11 +21,10 @@ classdef DWU < ALGORITHM
         function main(Algorithm,Problem)
             %% Generate random population
             Population = Problem.Initialization();
-            [Population,FrontNo] = EnvironmentalSelection(Population,Problem.N);
 
             %% Optimization
             while Algorithm.NotTerminated(Population)
-                MatingPool = TournamentSelection(2,Problem.N,FrontNo);
+                MatingPool = TournamentSelection(2,Problem.N,sum(max(0,Population.cons),2));
                 Offspring  = OperatorGA(Problem,Population(MatingPool));
                 [Population,FrontNo,] = EnvironmentalSelection([Population,Offspring],Problem.N);
             end
