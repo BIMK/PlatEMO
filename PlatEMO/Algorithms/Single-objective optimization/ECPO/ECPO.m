da551f130cc2344b337ebd791c2c58ba78037096
Fantasy-Dzx <1437749196@qq.com>
2024-04-07 22:03:10 +0800

    Update

---
+++ PlatEMO/Algorithms/Single-objective optimization/ECPO/ECPO.m
@@ -0,0 +1,51 @@
+classdef ECPO < ALGORITHM
+% <single> <real/integer> <large/none> <constrained/none>
+% Electric charged particles optimization
+% Strategy --- 2 --- Strategy
+% nECPI    --- 3 --- Number of ECPs in interaction
+% naECP    ---   --- Size of the archive pool
+
+%------------------------------- Reference --------------------------------
+% H. R. E. H. Bouchekara. Electric charged particles optimization and its
+% application to the optimal design of a circular antenna array.
+% Artificial Intelligence Review, 2021, 54: 1767-1802.
+%------------------------------- Copyright --------------------------------
+% Copyright (c) 2024 BIMK Group. You are free to use the PlatEMO for
+% research purposes. All publications which use this platform or any code
+% in the platform should acknowledge the use of "PlatEMO" and reference "Ye
+% Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB platform
+% for evolutionary multi-objective optimization [educational forum], IEEE
+% Computational Intelligence Magazine, 2017, 12(4): 73-87".
+%--------------------------------------------------------------------------
+
+% This function is written by H. R. E. H. Bouchekara (email: bouchekara.houssem@gmail.com)
+
+    methods
+        function main(Algorithm,Problem)
+            %% Parameter setting
+            [Strategy,nECPI,naECP] = Algorithm.ParameterSet(2,3,round(Problem.N/3));
+
+            %% Generate random population
+            ECP      = Problem.Initialization();
+            [~,rank] = sort(ECP.objs);
+            ECP      = ECP(rank);
+
+            if Strategy == 1
+                pop_fac = 2*nchoosek(nECPI,2);
+            elseif Strategy == 2
+                pop_fac = nECPI;
+            elseif Strategy == 3
+                pop_fac = 2*nchoosek(nECPI,2) + nECPI;
+            end
+
+            %% Optimization
+            while Algorithm.NotTerminated(ECP)
+                Archive   = ECP(1:naECP);
+                ECP_NEW   = OperatorECPO(Problem,Strategy,ECP,Archive,pop_fac,nECPI);
+                ECP_All   = [Archive, ECP_NEW];
+                [~,rank1] = sort(ECP_All.objs);
+                ECP       = ECP_All (rank1);
+            end
+        end
+    end
+end
---
+++ PlatEMO/Algorithms/Single-objective optimization/ECPO/OperatorECPO.m
@@ -0,0 +1,102 @@
+function ECP_NEW = OperatorECPO(Problem,Strategy,ECP,Archive,pop_fac,nECPI)
+%OperatorECPO - The operator of Electric Charged Particles Optimization.
+%
+%   ECP_NEW = OperatorECPO(Problem,Strategy,ECP,Archive,pop_fac,nECPI) 
+%
+%   Example:
+%       Off = OperatorECPO(Problem,Strategy,ECP,Archive,pop_fac,nECPI)
+
+%------------------------------- Copyright --------------------------------
+% Copyright (c) 2024 BIMK Group. You are free to use the PlatEMO for
+% research purposes. All publications which use this platform or any code
+% in the platform should acknowledge the use of "PlatEMO" and reference "Ye
+% Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB platform
+% for evolutionary multi-objective optimization [educational forum], IEEE
+% Computational Intelligence Magazine, 2017, 12(4): 73-87".
+%--------------------------------------------------------------------------
+
+% This function is written by H. R. E. H. Bouchekara (email: bouchekara.houssem@gmail.com)
+
+    %% Parameter setting
+    ECPDec             = ECP.decs;
+    ArchiveDEC         = Archive.decs;
+    [nECP,PROBLEMSIZE] = size(ECPDec);
+    [naECP,~]          = size(ArchiveDEC);
+
+    %% ECP optimization
+    ECPDec_NEW  = [];
+    ECPDec_NEW1 = [];
+    ECPDec_NEW2 = [];
+
+    for i = 1 : ceil(nECP/pop_fac)
+        % generate beta = Gaussian number, with mean=0.7 and standard
+        % deviation=0.2
+        Force = normrnd(0.7,0.2);
+
+        SP = sort(randperm(nECP,nECPI));
+
+        SP = SP(1:nECPI);
+
+        if Strategy == 1
+            for ii = 1 : nECPI
+                for jj = 1 : nECPI
+                    S1 = ECPDec(SP(ii),:)+ Force * (ECPDec(1,:) - ECPDec(SP(ii),:));
+                    if jj < ii
+                        S1 = S1 + Force * (ECPDec(SP(jj),:) - ECPDec(SP(ii),:));
+                        ECPDec_NEW(end+1,:) = S1;
+                    elseif jj > ii
+                        S1 = S1 - Force * (ECPDec(SP(jj),:) - ECPDec(SP(ii),:));
+                        ECPDec_NEW(end+1,:) = S1;
+                    end
+                end
+            end
+        elseif Strategy == 2
+            for ii = 1 : nECPI
+                S1 = ECPDec(SP(ii),:)+ 0*Force * (ECPDec(1,:) - ECPDec(SP(ii),:));
+                for jj = 1 : nECPI
+                    if jj < ii
+                        S1 = S1 + Force * (ECPDec(SP(jj),:) - ECPDec(SP(ii),:));
+                    elseif jj > ii
+                        S1 = S1 - Force * (ECPDec(SP(jj),:) - ECPDec(SP(ii),:));
+                    end
+                end
+                ECPDec_NEW(end+1,:) = S1;
+            end
+        elseif Strategy == 3
+            for ii = 1 : nECPI
+                S2 = ECPDec(SP(ii),:)+ Force * (ECPDec(1,:) - ECPDec(SP(ii),:));
+                for jj = 1 : nECPI
+                    S1 = ECPDec(SP(ii),:) + Force * (ECPDec(1,:) - ECPDec(SP(ii),:));
+                    if jj < ii
+                        S1 = S1 + Force * (ECPDec(SP(jj),:) - ECPDec(SP(ii),:));
+                        S2 = S2 + Force * (ECPDec(SP(jj),:) - ECPDec(SP(ii),:));
+                        ECPDec_NEW1(end+1,:) = S1;
+                    elseif jj > ii
+                        S1 = S1 - Force * (ECPDec(SP(jj),:) - ECPDec(SP(ii),:));
+                        S2 = S2 - Force * (ECPDec(SP(jj),:) - ECPDec(SP(ii),:));
+                        ECPDec_NEW1(end+1,:) = S1;
+                    end
+                end
+                ECPDec_NEW2(end+1,:) = S2;
+            end
+            ECPDec_NEW = [ECPDec_NEW1; ECPDec_NEW2];
+        end
+    end
+
+    ECPDec_NEW = bound(Problem,ECPDec_NEW);
+
+    for i1 = 1 : size(ECPDec_NEW,1)
+        for j = 1 : PROBLEMSIZE
+            r = rand;
+            if r < 0.2
+                pos = randi(naECP(1));
+                ECPDec_NEW(i1,j) = ArchiveDEC(pos,j);
+            end
+        end
+    end
+    ECP_NEW = Problem.Evaluation(ECPDec_NEW);
+end
+
+function x = bound(Problem,x)
+    x = min(max(x,Problem.lower),Problem.upper);
+end
